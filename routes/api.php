0582b99fb0aafb3807a9ed4111dfda872ec11112
LeDuyNe <leduy.19102000@gmail.com>
2022-10-04 00:37:38 +0700

    add protectec

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\AdminController;
+use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\API\CreatorController;
 use App\Http\Controllers\API\ExamineesController;
